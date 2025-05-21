da851a1b0dd370d3a5ec13fb8f01f58a7ff55804
AshokNandu-11 <114278147+AshokNandu-11@users.noreply.github.com>
2025-05-21 09:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById('form1');
+	const fName = form.fname.value.trim();
+	const lName = form.lname.value.trim();
+
+	alert(fName +" "+lName);
 
 }
